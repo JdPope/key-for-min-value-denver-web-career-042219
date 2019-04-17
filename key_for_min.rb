89a70562ab87ee4ba3fec1480d03296d960fae8a
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-17 16:52:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
 key_with_lowest = nil
 name_hash.collect do |key, value|
-if key_with_lowest = nil
+if key_with_lowest == nil
   key_with_lowest = key
 elsif value<name_hash[key_with_lowest]
   key_with_lowest = key
